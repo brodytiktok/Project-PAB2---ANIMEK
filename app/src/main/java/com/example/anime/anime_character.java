dfe92df0348051d140c2f2a6417596bbdd935401
brodytiktok <105333102+brodytiktok@users.noreply.github.com>
2022-12-15 13:52:46 +0700

    a

---
+++ app/src/main/java/com/example/anime/anime_character.java
@@ -0,0 +1,14 @@
+package com.example.anime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class anime_character extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime_character);
+    }
+}
---
+++ app/src/main/java/com/example/anime/anime_info.java
@@ -0,0 +1,14 @@
+package com.example.anime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class anime_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime_info);
+    }
+}
