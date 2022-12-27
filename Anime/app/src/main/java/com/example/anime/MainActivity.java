e6827b0aed2019a65e8c5f242ad8ed4708e83002
hendranataniko <nickhend02@Gmail.com>
2022-12-27 16:30:20 +0700

    .

--- Anime/app/src/main/java/com/example/anime/MainActivity.java
+++ Anime/app/src/main/java/com/example/anime/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.anime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.anime.databinding.ActivityMainBinding;
+import com.example.anime.databinding.ActivitySignUpBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnForum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
