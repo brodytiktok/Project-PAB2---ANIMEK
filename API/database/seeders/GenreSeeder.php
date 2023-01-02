dd9e49be1362a88005db3b55681c40cbfbd6499e
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-01-02 14:47:52 +0700

    AKHER NY KELAR SEEDER

--- API/database/seeders/GenreSeeder.php
+++ API/database/seeders/GenreSeeder.php
@@ -15,12 +15,38 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('genres')->insert([
+        DB::table('genres')->insert([//1
             'nama_genre'=>'Action'
         ]);
-
-        DB::table('genres')->insert([
+        DB::table('genres')->insert([//2
             'nama_genre'=>'Comedy'
+        ]);
+        DB::table('genres')->insert([//3
+            'nama_genre'=>'Adventure'
+        ]);
+        DB::table('genres')->insert([//4
+            'nama_genre'=>'Drama'
+        ]);
+        DB::table('genres')->insert([//5
+            'nama_genre'=>'Fantasy'
+        ]);
+        DB::table('genres')->insert([//6
+            'nama_genre'=>'Sci-Fi'
+        ]);
+        DB::table('genres')->insert([//7
+            'nama_genre'=>'Suspense'
+        ]);
+        DB::table('genres')->insert([//8
+            'nama_genre'=>'Supernatural'
+        ]);
+        DB::table('genres')->insert([//9
+            'nama_genre'=>'Sports'
+        ]);
+        DB::table('genres')->insert([//10
+            'nama_genre'=>'Mystery'
+        ]);
+        DB::table('genres')->insert([//11
+            'nama_genre'=>'Slice of Life'
         ]);
     }
 }
