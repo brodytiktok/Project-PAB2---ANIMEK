c7810bdaefd1e38c7dc01338622e85f65a08457b
chizururu <121595508+chizururu@users.noreply.github.com>
2023-01-09 04:41:41 +0700

    a

--- Anime/app/src/main/java/com/example/anime/adapter/AdapterAnime.java
+++ Anime/app/src/main/java/com/example/anime/adapter/AdapterAnime.java
@@ -1,32 +1,69 @@
 package com.example.anime.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.anime.databinding.ListAnimeBinding;
+import com.example.anime.model.API.APIResponse;
+import com.example.anime.model.API.Anime;
+
+import java.util.List;
+
 public class AdapterAnime extends RecyclerView.Adapter<AdapterAnime.ViewHolder> {
+    private APIResponse<List<Anime>> anime;
+    private ItemClickListener<Anime> itemClickListener;
+
+    public interface ItemClickListener<T> {
+        void itemClick(T data, int position);
+    }
+    public AdapterAnime(ItemClickListener<Anime> itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public void setAnimeAdapter(APIResponse<List<Anime>> anime) {
+        this.anime = anime;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(ListAnimeBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterAnime.ViewHolder holder, int position) {
-        int amn = holder.getAdapterPosition()
+        Anime result = anime.getData().get(position);
+        holder.binding.tvAnime.setText("Anime : "+ result.getJudul_anime());
+        Glide.with(holder.itemView.getContext())
+                .load(result.getFoto_anime())
+                .into(holder.binding.ivAnime);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemClickListener.itemClick(result, position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return anime.getData().size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        private ListAnimeBinding binding;
+        public ViewHolder(@NonNull ListAnimeBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
         }
     }
 }
--- Anime/app/src/main/java/com/example/anime/adapter/AdapterCharacters.java
+++ Anime/app/src/main/java/com/example/anime/adapter/AdapterCharacters.java
@@ -8,8 +8,12 @@ import android.widget.AdapterView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.anime.R;
+import com.example.anime.databinding.ListCharactersBinding;
+import com.example.anime.model.API.APIResponse;
 import com.example.anime.model.API.APIUtil;
+import com.example.anime.model.API.AnimeCharacter;
 
 import java.util.List;
 
@@ -40,40 +44,39 @@ import java.util.List;
 //    }
 //}
 public class AdapterCharacters extends RecyclerView.Adapter<AdapterCharacters.ListViewHolder {
-    private APIUtil<List<Character>> character;
-    private AdapterView.OnItemClickListener<Character> itemClickListener;
+    private APIResponse<List<AnimeCharacter>> character;
 
-    public AdapterCharacters(AdapterView.OnItemClickListener itemClickListener) {
-        this.itemClickListener = itemClickListener;
-    }
-
-    public void setCharacter(APIUtil<List<Character>> character) {
+    public void setCharacter(APIResponse<List<AnimeCharacter>> character) {
         this.character = character;
         notifyDataSetChanged();
     }
 
+
     @NonNull
     @Override
     public AdapterCharacters.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_characters, parent, false);
-
-        return new ListViewHolder(view);
+        return new ListViewHolder(ListCharactersBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterCharacters.ListViewHolder holder, int position) {
-
+        AnimeCharacter result = character.getData().get(position);
+        holder.binding.tvCharacter.setText(result.getNama_character());
+        Glide.with(holder.itemView.getContext())
+                .load(result.getFoto_seiyuu())// UBAH LAGI APO DIO FOTO SEIYUU GANTI DI MODEL :V
+                .into(holder.binding.ivCharacter);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return character.getData().size();
     }
 
     public class ListViewHolder extends  RecyclerView.ViewHolder{
-        public ListViewHolder(View view) {
-            super(view);
-
+        private ListCharactersBinding binding;
+        public ListViewHolder(@NonNull ListCharactersBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
