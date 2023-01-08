c680393058425d5f07357fc4d71a75ca8f1a9059
chizururu <121595508+chizururu@users.noreply.github.com>
2023-01-08 14:13:42 +0700

    aa

--- Anime/app/src/main/java/com/example/anime/MainActivity.java
+++ Anime/app/src/main/java/com/example/anime/MainActivity.java
@@ -1,24 +1,17 @@
 package com.example.anime;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.example.anime.databinding.ActivityMainBinding;
-import com.example.anime.databinding.ActivitySignUpBinding;
-import com.example.anime.fragment.AnimeFragment;
-import com.example.anime.fragment.ForumFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -34,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
 
-        openFragment(new AnimeFragment());
+        openFragment(new FragmentAnime());
         getSupportActionBar().setTitle("Anime");
 
         bvmmenu = findViewById(R.id.bnvmenu);
@@ -45,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
                 Fragment FR;
                 switch (item.getItemId()) {
                     case R.id.menu_anime:
-                        openFragment(new AnimeFragment());
+                        openFragment(new FragmentAnime());
 //                        getActionBar().setTitle("Anime");
                         return true;
                     case R.id.menu_forum:
-                        openFragment(new ForumFragment());
+                        openFragment(new FragmentForum());
 //                        getActionBar().setTitle("Forum");
                         return true;
                 }
